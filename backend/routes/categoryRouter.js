cb42f89da9b5a969eaab58b1044f6ab6a9677fc3
Bushraamarat <amratbushra@gmail.com>
2024-01-10 13:43:01 +0300

    Commit#11

--- backend/routes/categoryRouter.js
+++ backend/routes/categoryRouter.js
@@ -2,5 +2,5 @@ const express=require("express")
 const categoryRouter=express.Router();
 const{createNewCategory}=require("../controllers/categoryController");
 
-categoryRouter.post("/new",createNewCategory);
+categoryRouter.post("/newcategory",createNewCategory);
 module.exports=categoryRouter;
